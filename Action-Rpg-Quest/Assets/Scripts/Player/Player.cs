7175cfaa064c663d7b4981f6cf03ad98a600a1b1
frnce <francetag32@gmail.com>
2019-04-11 00:07:14 +0800

    Init Project

---
+++ Action-Rpg-Quest/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Advent.Controller;
+
+namespace Advent.Player
+{
+    public class Player : MonoBehaviour
+    {
+        public static Player instance;
+
+        [SerializeField]
+        private float movementSpeed = 10f;
+        private bool isMoving = true;
+        private Rigidbody2D rb2d;
+        private PlayerController playerControls = null;
+        private void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Debug.LogWarning("two or more instance of " + instance.gameObject.name + " detected.");
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerControls = PlayerController.instance;
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+        private void Movement()
+        {
+            int xDir = playerControls.GetXMovement();
+            int yDir = playerControls.GetYMovement();
+            if (xDir != 0 || yDir != 0)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
+            rb2d.velocity = new Vector2(Mathf.Lerp(0, xDir * movementSpeed, 0.8f), //Change MovementSpeed to something from stats
+                                               Mathf.Lerp(0, yDir * movementSpeed, 0.8f));
+        }
+    }
+}
---
+++ Action-Rpg-Quest/Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent.Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public static PlayerController instance;
+        private int xMove;
+        private int yMove;
+        //private readonly bool acceptKey;
+        //private readonly bool cancelKey;
+        //private readonly bool action1;
+        //private readonly bool action2;
+        //private readonly bool action3;
+        private void Awake()
+        {
+            instance = this;
+        }
+        private void Start()
+        {
+            Movement();
+        }
+        void Update()
+        {
+            Movement();
+        }
+        private void Movement()
+        {
+            xMove = (int)Input.GetAxisRaw("Horizontal");
+            yMove = (int)Input.GetAxisRaw("Vertical");
+        }
+
+        public int GetXMovement()
+        {
+            return xMove;
+        }
+        public int GetYMovement()
+        {
+            return yMove;
+        }
+        //public bool GetAcceptKey()
+        //{
+        //    return acceptKey;
+        //}
+        //public bool GetCancelKey()
+        //{
+        //    return cancelKey;
+        //}
+    }
+}
