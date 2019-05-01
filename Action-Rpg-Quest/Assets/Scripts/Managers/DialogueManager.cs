3c5d32a9dfbc446616900fd08916bc09ed7175a0
frnce <francetag32@gmail.com>
2019-05-01 17:08:10 +0800

    levelselect

--- Action-Rpg-Quest/Assets/Scripts/Managers/DialogueManager.cs
+++ Action-Rpg-Quest/Assets/Scripts/Managers/DialogueManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace Advent.Manager
 {
@@ -20,16 +22,24 @@ namespace Advent.Manager
             }
             DontDestroyOnLoad(gameObject);
         }
+        [SerializeField]
+        private TMP_Text dialogText = null;
+        [SerializeField]
+        private TMP_Text dialogNameText = null;
+        [SerializeField]
+        private GameObject dialogPanel = null;
         private Queue<string> sentences;
 
         private void Start()
         {
             sentences = new Queue<string>();
+            dialogPanel.SetActive(false);
         }
 
         public void StartDialogue(Dialogue dialogue)
         {
-            Debug.Log("Starting convo with " + dialogue.name);
+            dialogPanel.SetActive(true);
+            dialogNameText.text = dialogue.name;
 
             sentences.Clear();
 
@@ -48,11 +58,22 @@ namespace Advent.Manager
                 return;
             }
             string sentence = sentences.Dequeue();
-            Debug.Log(sentence);
+            StopAllCoroutines();
+            StartCoroutine(TypeEffectText(sentence));
+        }
+        private IEnumerator TypeEffectText(string sentence)
+        {
+            dialogText.text = "";
+            foreach (char letter in sentence.ToCharArray())
+            {
+                dialogText.text += letter;
+                yield return null;
+            }
         }
         void EndDialogue()
         {
             Debug.Log("End of convo");
+            dialogPanel.SetActive(false);
         }
     }
 }
--- Action-Rpg-Quest/Assets/Scripts/Managers/GameManager.cs
+++ Action-Rpg-Quest/Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 namespace Advent.Manager
 {
+    [System.Serializable]
+    public class Level
+    {
+        public string sceneName;
+        public string levelTitle;
+        public bool isUnlocked;
+
+        public Level(string _sceneName, string _levelTitle, bool _isUnlocked)
+        {
+            sceneName = _sceneName;
+            levelTitle = _levelTitle;
+            isUnlocked = _isUnlocked;
+        }
+    }
     public class GameManager : MonoBehaviour
     {
         public static GameManager instance;
@@ -18,17 +32,35 @@ namespace Advent.Manager
                 instance = this;
             }
             DontDestroyOnLoad(gameObject);
+
+            GetAllLevels();
         }
+
+        public List<Level> levels = new List<Level>();
         // Start is called before the first frame update
-        void Start()
+        private void GetAllLevels()
         {
-
+            int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+            for (int i = 1; i < sceneCount; i++)
+            {
+                string scene = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+                string[] splittedName = scene.Split('_');
+                if (scene.Contains("Level"))
+                {
+                    levels.Add(new Level(scene,splittedName[1], false));
+                }
+            }
         }
-
-        // Update is called once per frame
-        void Update()
+        public void UnlockInstance(string levelName)
         {
-
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levels[i].sceneName.Contains(levelName))
+                {
+                    levels[i].isUnlocked = true;
+                    return;
+                }
+            }
         }
     }
 
